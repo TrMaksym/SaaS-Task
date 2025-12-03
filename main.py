a66d77fc177fdb43fde5bd92c9384d397d6a1f13
Maximus Champion <maximuschampion2002@gmail.com>
2025-12-03 14:41:42 +0100

    connect database and create models

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 
 from routers import users
-from routers import movies
+from routers import tasks
 
 app = FastAPI()
 
 app.include_router(users.router)
-app.include_router(movies.router)
+app.include_router(tasks.router)
 
 
 @app.get("/")
---
+++ models.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from database import Base
+from datetime import datetime
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, unique=True, nullable=False, index=True)
+    is_active = Column(Boolean, default=True)
+
+    tasks = relationship("Task", back_populates="owner")
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow)
+    is_done = Column(Boolean, default=False)
+    owner = relationship("User", back_populates="tasks")
