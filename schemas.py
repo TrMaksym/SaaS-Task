56ab40271405e496f97068430bf2d6c3495f0259
Maximus Champion <maximuschampion2002@gmail.com>
2025-12-13 12:03:54 +0100

    update schemas

--- schemas.py
+++ schemas.py
@@ -1,10 +1,11 @@
-from pydantic import BaseModel, EmailStr
-from typing import Optional
+from pydantic import BaseModel
 from datetime import datetime
+from typing import Optional
+from models import TaskStatus
 
-
+# ----------------- User -----------------
 class UserBase(BaseModel):
-    email: EmailStr
+    email: str
 
 class UserCreate(UserBase):
     password: str
@@ -13,18 +14,28 @@ class UserRead(UserBase):
     id: int
     is_active: bool
 
+    class Config:
+        orm_mode = True
+
+# ----------------- Task -----------------
 class TaskBase(BaseModel):
     title: str
-    description: Optional[str]
+    description: Optional[str] = None
 
 class TaskCreate(TaskBase):
-    pass
+    status: Optional[TaskStatus] = TaskStatus.TODO
+
+class TaskUpdate(BaseModel):
+    title: Optional[str]
+    description: Optional[str]
+    status: Optional[TaskStatus]
 
 class TaskRead(TaskBase):
     id: int
     owner_id: int
+    status: TaskStatus
     created_at: datetime
-    is_done: bool
+    updated_at: datetime
 
     class Config:
         orm_mode = True
