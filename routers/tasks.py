27fd154c1bbf339f397e3ac45e408313eb5cb755
Maximus Champion <maximuschampion2002@gmail.com>
2025-12-13 11:47:39 +0100

    add task creation linked to authenticated user

--- routers/tasks.py
+++ routers/tasks.py
@@ -3,12 +3,22 @@ from sqlalchemy.orm import Session
 from sqlalchemy.sql.functions import current_user
 
 import crud, schemas
+import models
 from database import get_db
+from dependencies import get_current_user
 
 router = APIRouter(
     prefix="/users",
 )
 
 @router.post("/", response_model=schemas.TaskRead)
-def create_task(task: schemas.TaskCreate,user_id: int, db: Session = Depends(get_db)):
-    return crud.create_task(task=task, db=db, user_id=current_user.id)
+def create_task(
+    task: schemas.TaskCreate,
+    current_user: models.User = Depends(get_current_user),
+    db: Session = Depends(get_db)
+):
+    return crud.create_task(
+        db=db,
+        task=task,
+        user_id=current_user.id
+    )
