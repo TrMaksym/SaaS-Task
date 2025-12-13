137bb1134e35d3eeb49c2a67f2efb09eaff77c65
Maximus Champion <maximuschampion2002@gmail.com>
2025-12-13 12:02:18 +0100

    added status for tasks

--- models.py
+++ models.py
@@ -1,17 +1,27 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime
+import enum
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime, Enum
 from sqlalchemy.orm import relationship
 from database import Base
-from datetime import datetime
+
+
+class TaskStatus(str, enum.Enum):
+    TODO = "TODO"
+    IN_PROGRESS = "IN_PROGRESS"
+    DONE = "DONE"
+
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
     email = Column(String, unique=True, nullable=False, index=True)
-    is_active = Column(Boolean, default=True)
     hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True)
 
     tasks = relationship("Task", back_populates="owner")
+
 
 class Task(Base):
     __tablename__ = "tasks"
@@ -23,4 +33,6 @@ class Task(Base):
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow)
     is_done = Column(Boolean, default=False)
+    status = Column(Enum(TaskStatus), default=TaskStatus.TODO)
+
     owner = relationship("User", back_populates="tasks")
