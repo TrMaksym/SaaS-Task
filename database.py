1f059c2643ae5ea557efa58cca8871ced61999f1
Maximus Champion <maximuschampion2002@gmail.com>
2026-01-06 15:30:40 +0100

    Set DATABASE_URL exclusively via environment variable

--- database.py
+++ database.py
@@ -4,9 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.getenv(
-    "DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/postgres"
-)
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
