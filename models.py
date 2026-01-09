7826e9ae22452af1c765fb95521b3e0aeabe6ef1
Maximus Champion <maximuschampion2002@gmail.com>
2026-01-09 19:37:43 +0100

    Add 'role' field to User model

--- models.py
+++ models.py
@@ -98,6 +98,7 @@ class User(Base):
     email = Column(String, unique=True, nullable=False, index=True)
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
+    role = Column(Enum(TeamRole), default=TeamRole.MEMBER)
 
     tasks = relationship("Task", back_populates="owner")
     assigned_tasks = relationship(
